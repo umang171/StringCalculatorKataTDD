b363b979bd9d13ecba4d596930a0fc6fc17d04e9
Umang Gohel <umanggohel17@gmail.com>
2022-08-20 22:26:26 +0530

    Add test that checks number is two digit number

--- string_calculator.py
+++ string_calculator.py
@@ -4,3 +4,7 @@ class StringCalculator:
             return 0
         elif(len(numbers.strip()) == 1):
             return int(numbers)
+        elif(numbers.strip().count(",") == 1):
+            numbers = numbers.strip().split(",")
+            numbers = list(map(lambda x: int(x), numbers))
+            return sum(numbers)
--- test_string_calculator.py
+++ test_string_calculator.py
@@ -13,3 +13,7 @@ class TestStringCalculator(unittest.TestCase):
     def test_number_is_one_digit(self):
         self.assertEqual(self.obj.add("1"), 1)
         self.assertEqual(self.obj.add("3"), 3)
+
+    def test_number_is_two_digit(self):
+        self.assertEqual(self.obj.add("1,2"), 3)
+        self.assertEqual(self.obj.add("13,7"), 20)
